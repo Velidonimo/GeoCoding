5686fbe18df04b6d7b382d01cb5c6ede8dc15297
Velidonimo <velidon@mail.ru>
2020-12-05 22:42:33 +0300

    Upload and download done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pandas
 
 
@@ -12,10 +12,25 @@ def index():
 @app.route('/success/', methods=["POST"])
 def success():
     if request.method == "POST":
+        global file
         file = request.files['file_name']
         df = pandas.read_csv(file)
+        return render_template('success.html',
+                data_frame=df.to_html(max_rows=10, max_cols=8, classes="table"))
 
-        return render_template('success.html', dataframe=df.to_html())
+
+@app.route("/file_download/")
+def file_download():
+    # adding "+coords" to filename
+    new_name = ".".join(file.filename.split(".")[:-1]) + "+coords.csv"
+
+    file_to_download = send_file(file.filename, attachment_filename=new_name, as_attachment=True)
+    # clearing cache
+    file_to_download.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
+
+    # TODO add new btn to return to main page
+
+    return file_to_download
 
 
 if __name__ == '__main__':
