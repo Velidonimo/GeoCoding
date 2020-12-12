bc92117b3845ac11abbde9e1bb7a0940322f8d35
Velidonimo <velidon@mail.ru>
2020-12-12 21:09:30 +0300

    processing doesnt work

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 from flask import Flask, render_template, request, send_file
-import pandas
 from werkzeug.utils import secure_filename
+from threading import Thread
+from coords_finder import coords_finder
 
 
-app = Flask("__main__")
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/success/', methods=["POST"])
-def success():
+@app.route('/updating/', methods=["POST"])
+def updating():
     if request.method == "POST":
-        global file
         file = request.files['file_name']
-        df = pandas.read_csv(file)
-        print("Address" in df)
-        return render_template('success.html',
-                data_frame=df.to_html(max_rows=10, max_cols=8, classes="table"))
+        global filename
+        filename = secure_filename(file.filename)
+        thread = Thread(target=coords_finder, args=(filename, success))
+        thread.start()
+        return render_template('updating.html')
+
+
+@app.route('/success/')
+def success(done, message):
+    return render_template('success.html', data_frame=message, show_btn=done)
 
 
 @app.route("/file_download/")
 def file_download():
     # adding "+coords" to filename
-    filename = secure_filename(file.filename)
     new_name = ".".join(filename.split(".")[:-1]) + "+Coords.csv"
-
-    file_to_download = send_file(filename, attachment_filename=new_name, as_attachment=True)
+    file_to_download = send_file("Nice.csv", attachment_filename=new_name, as_attachment=True)
     # clearing cache
     file_to_download.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
 
--- coords_finder.py
+++ coords_finder.py
@@ -2,14 +2,22 @@ import pandas
 from geopy.geocoders import Nominatim
 
 
-def coords_finder(filename):
+def coords_finder(filename, succsess_page):
+    """
+    Finds and adds latitude and longitude to the file. Also saves it as Nice.csv
+    :param filename: a path to file
+    :return: if no problem: (True, HTML repr of df)
+             else: (False, Error string to show on page)
+    """
     try:
         df = pandas.read_csv(filename)
     except:
-        return False, "Can't open Your file. Please ensure, You are uploading a .csv file"
+        succsess_page(False, "Can't open Your file. Please ensure, You are uploading a .csv file")
+        return
 
     if not ("Address" in df or "address" in df):
-        return False, 'No address column. Please ensure, Your file has a column "Address" or "address"'
+        succsess_page(False, 'No address column. Please ensure, Your file has a column "Address" or "address"')
+        return
 
     df["coords"] = df.Address.apply(Nominatim(user_agent="agent").geocode)
     df["Latitude"] = df.coords.apply(lambda x: x.latitude if x != None else None)
@@ -21,10 +29,11 @@ def coords_finder(filename):
 
     try:
         df.to_csv('Nice.csv')
-    except PermissionError:
-        return (False, "Can't save the file")
+    except:
+        succsess_page(False, "Can't proceed this file")
+        return
 
-    return True, "OK"
+    succsess_page(True, df.to_html(max_rows=10, max_cols=8, classes="table"))
 
 
 if __name__ == '__main__':
